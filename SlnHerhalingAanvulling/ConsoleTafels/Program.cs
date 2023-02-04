8f3616c3871c65081ef81d358d6b043e8c544b22
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-04 15:09:10 +0100

    console

--- SlnHerhalingAanvulling/ConsoleTafels/Program.cs
+++ SlnHerhalingAanvulling/ConsoleTafels/Program.cs
@@ -8,8 +8,49 @@ namespace ConsoleTafels
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+
+ 
+            Console.WriteLine("4x8 tafel: ");
+            DrukTafel(4, 8);
+            Console.WriteLine("\n2x5 tafel: ");
+            DrukTafel(2, 5);
+
+
+            Console.Write("\nGeef een getal:");
+            int getal = VraagPositiefGetal();
+            Console.Write("Geef de lengte van de tafel:");
+            int lengte = VraagPositiefGetal();
+            Console.WriteLine("Tafel van " + getal + ":");
+            DrukTafel(getal, lengte);
+            Console.ReadKey(true);
+
         }
+
+        static void DrukTafel(int getal, int lengte )
+        {
+            int result;
+            for(int i = 1; i <= lengte; i++)
+            {
+                result = getal * i;
+                Console.WriteLine($"{ getal} x {i} = {result}");
+            }
+           
+        }
+
+        static int VraagPositiefGetal()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result) || result <= 0)
+            {
+                Console.WriteLine("Ongeldige invoer, geef een positief getal:");
+            }
+            return result;
+            
+        }
+        
+        
     }
 }
