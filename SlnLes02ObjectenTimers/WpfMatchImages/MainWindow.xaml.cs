1e56078a876e24f5f2cb967f0b20cbfc44522dd6
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-10 16:54:00 +0100

    trial

--- SlnLes02ObjectenTimers/WpfMatchImages/MainWindow.xaml.cs
+++ SlnLes02ObjectenTimers/WpfMatchImages/MainWindow.xaml.cs
@@ -41,6 +41,55 @@ namespace WpfMatchImages
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             watch.Start();
+            Button clickedButton = (Button)sender;
+
+            while (clickedButton != null)
+            {
+                if (clickedButton.Tag == hiveB.Tag)
+                {
+                    beeIm.Opacity = 0.5;
+                    hiveIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == dogB.Tag)
+                {
+                    leashIm.Opacity = 0.5;
+                    dogIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == catB.Tag)
+                {
+                    catIm.Opacity = 0.5;
+                    postB.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == penguinB.Tag)
+                {
+                    penguinIm.Opacity = 0.5;
+                    snowIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == owlB.Tag)
+                {
+                    owlIm.Opacity = 0.5;
+                    nightIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == cowB.Tag)
+                {
+                    cowIm.Opacity = 0.5;
+                    milkIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == chickenB.Tag)
+                {
+                    chickenIm.Opacity = 0.5;
+                    eegIm.Opacity = 0.5;
+                }
+                if (clickedButton.Tag == birdB.Tag)
+                {
+                    birdIm.Opacity = 0.5;
+                    houseIm.Opacity = 0.5;
+                }
+
+            }
+
+
+
             lblTimer.Content = watch;
         }
         private void DoSomething(object sender, EventArgs e)
