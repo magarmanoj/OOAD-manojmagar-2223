7de629fdccf09b5244e8d5f3c6dad70fef11c5d6
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-10 16:37:07 +0100

    try

--- SlnLes02ObjectenTimers/WpfMatchImages/MainWindow.xaml.cs
+++ SlnLes02ObjectenTimers/WpfMatchImages/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfMatchImages
 {
@@ -20,14 +22,33 @@ namespace WpfMatchImages
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Stopwatch watch = new Stopwatch();
+        private int ticks;
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            ticks = 0;
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(200);
+            timer.Tick += DoSomething;
+            timer.Start();
+
         }
 
-        private void Button_Click()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            watch.Start();
+            lblTimer.Content = watch;
+        }
+        private void DoSomething(object sender, EventArgs e)
+        {
+            // elke 200 milliseconden uitgevoerd
+            ticks++;
+            // lblTimer.Content = ticks;
+            
         }
     }
 }
