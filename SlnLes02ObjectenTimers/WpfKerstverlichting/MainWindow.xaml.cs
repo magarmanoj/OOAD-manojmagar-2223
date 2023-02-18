8b9de26f17f27e3fbc6f4a3786d9f5782d4fca9b
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-18 14:36:27 +0100

    code uit ppt

--- SlnLes02ObjectenTimers/WpfKerstverlichting/MainWindow.xaml.cs
+++ SlnLes02ObjectenTimers/WpfKerstverlichting/MainWindow.xaml.cs
@@ -22,7 +22,34 @@ namespace WpfKerstverlichting
     {
         public MainWindow()
         {
+            // code uit ppt
             InitializeComponent();
+
+            // maak het lampje met een ellips (cirkel)
+            Ellipse newLight = new Ellipse();
+            newLight.Width = 10;
+            newLight.Height = 10;
+            newLight.Fill = Brushes.Gray;
+            newLight.Stroke = Brushes.Black;
+            double xPos = 150;
+            double yPos = 285;
+            newLight.SetValue(Canvas.LeftProperty, (double)xPos);
+            newLight.SetValue(Canvas.TopProperty, (double)yPos);
+
+            // voeg ellips toe aan het canvas
+            cnvTree.Children.Add(newLight);
         }
+
+        public static bool PixelIsWhite(Image img, int x, int y)
+        {
+            BitmapSource source = img.Source as BitmapSource;
+            Color color = Colors.White;
+            CroppedBitmap cb = new CroppedBitmap(source, new Int32Rect(x, y, 1, 1));
+            byte[] pixels = new byte[4];
+            cb.CopyPixels(pixels, 4, 0);
+            color = Color.FromRgb(pixels[2], pixels[1], pixels[0]);
+            return color.ToString() == "#FFFFFFFF";
+        }
+
     }
 }
