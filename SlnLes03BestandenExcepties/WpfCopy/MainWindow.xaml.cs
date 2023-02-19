10cd2c7f9258ad795537cc37f23cc727266abf53
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-19 15:31:01 +0100

    wpfCopy

---
+++ SlnLes03BestandenExcepties/WpfCopy/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace WpfCopy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> newString = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKies_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            string chosenFileName;
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
+            {
+                // user picked a file and pressed OK
+                chosenFileName = dialog.FileName;
+                txtBoxvenster.Text = chosenFileName;              
+            }
+        }
+
+        private void BtnGo_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            dialog.FileName = "savedfile.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, "tekstinhoud hier");
+                lblMsg.Content = "bestand is overgezet";
+                btnGo.IsEnabled = false;
+            }
+        }
+    }
+}
