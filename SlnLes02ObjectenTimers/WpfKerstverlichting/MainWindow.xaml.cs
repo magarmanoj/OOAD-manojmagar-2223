c5fcf39c81b03fced8a245418743084f0e6233d1
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-21 15:30:26 +0100

    kerstverlichting

--- SlnLes02ObjectenTimers/WpfKerstverlichting/MainWindow.xaml.cs
+++ SlnLes02ObjectenTimers/WpfKerstverlichting/MainWindow.xaml.cs
@@ -9,9 +9,13 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Linq;
 
 namespace WpfKerstverlichting
 {
@@ -20,24 +24,44 @@ namespace WpfKerstverlichting
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private bool lightsON = false;
+        private List<Ellipse> lights = new List<Ellipse>();
+
         public MainWindow()
         {
             // code uit ppt
             InitializeComponent();
+            Random rand = new Random();
 
             // maak het lampje met een ellips (cirkel)
-            Ellipse newLight = new Ellipse();
-            newLight.Width = 10;
-            newLight.Height = 10;
-            newLight.Fill = Brushes.Gray;
-            newLight.Stroke = Brushes.Black;
-            double xPos = 150;
-            double yPos = 285;
-            newLight.SetValue(Canvas.LeftProperty, (double)xPos);
-            newLight.SetValue(Canvas.TopProperty, (double)yPos);
+            for (int i = 0; i < 40; i++)
+            {
+                // maak het lampje met een ellips (cirkel)
+                Ellipse newLight = new Ellipse();
+                newLight.Width = 10;
+                newLight.Height = 10;
+                newLight.Fill = Brushes.Gray;
+                newLight.Stroke = Brushes.Black;
 
-            // voeg ellips toe aan het canvas
-            cnvTree.Children.Add(newLight);
+                double xPos = rand.Next(0, 300);
+                double yPos = rand.Next(0, 425);
+
+                // while loop die 40 nieuwe licht na kijkt of het in witte pixel ligt of niet en al het ligt terug een nieuwe random coorinaat creeêren
+                while (PixelIsWhite(imgTree, (int)xPos, (int)yPos))
+                {
+                    xPos = rand.Next(0, 300);
+                    yPos = rand.Next(0, 425);
+                }
+
+                newLight.SetValue(Canvas.LeftProperty, (double)xPos);
+                newLight.SetValue(Canvas.TopProperty, (double)yPos);
+
+                // voeg ellips toe aan het canvas
+                cnvTree.Children.Add(newLight);
+                lights.Add(newLight);
+            }
+
         }
 
         public static bool PixelIsWhite(Image img, int x, int y)
@@ -50,6 +74,82 @@ namespace WpfKerstverlichting
             color = Color.FromRgb(pixels[2], pixels[1], pixels[0]);
             return color.ToString() == "#FFFFFFFF";
         }
+
+        private void SwitchOnOff_Click(object sender, RoutedEventArgs e)
+        {
+            if (!lightsON)
+            {
+                lightsON = true;
+                btnSwitch.Content = "Switch Off";
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(500);
+                timer.Tick += ColorChange;
+                timer.Start();
+            }
+            else
+            {
+                lightsON = false;
+                btnSwitch.Content = "Switch On";
+                timer.Stop();
+                foreach (Ellipse light in cnvTree.Children.OfType<Ellipse>())
+                {
+                    light.Fill = Brushes.Gray;
+                }
+            }     
+        }
+
+        // randomize de kleueren
+        private void ColorChange(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            foreach (Ellipse light in lights)
+            {
+                List<Brush> colors = new List<Brush>() { Brushes.Gray, Brushes.White, Brushes.Red, Brushes.Yellow, Brushes.Blue };
+                if (rand.NextDouble() >= 0.5)
+                {
+                    light.Fill = colors[rand.Next(0, colors.Count)];
+
+                }
+                else
+                {
+                    light.Fill = colors[rand.Next(0, colors.Count)];
+                }
+            }
+        }
+
+
+        // chatgpt om te kijken hoe je andere soort animatie kan toevoegen
+        //private void ColorChange(object sender, EventArgs e)
+        //{
+        //    Random rand = new Random();
+        //    foreach (Ellipse light in cnvTree.Children.OfType<Ellipse>())
+        //    {
+        //        List<Brush> colors = new List<Brush>() { Brushes.Gray, Brushes.White, Brushes.Red, Brushes.Yellow, Brushes.Blue };
+        //        if (rand.NextDouble() >= 0.5)
+        //        {
+        //            light.Fill = colors[rand.Next(0, colors.Count)];
+        //            DoubleAnimation animation = new DoubleAnimation();
+        //            animation.From = 0.0;
+        //            animation.To = 1.0;
+        //            animation.AutoReverse = true;
+        //            animation.Duration = TimeSpan.FromSeconds(5);
+        //            animation.RepeatBehavior = RepeatBehavior.Forever;
+        //            light.BeginAnimation(Ellipse.OpacityProperty, animation);
+        //        }
+        //        else
+        //        {
+        //            light.Fill = colors[rand.Next(0, colors.Count)];
+        //            DoubleAnimation animation = new DoubleAnimation();
+        //            animation.From = 1.0;
+        //            animation.To = 0.0;
+        //            animation.AutoReverse = true;
+        //            animation.Duration = TimeSpan.FromSeconds(0.5);
+        //            animation.RepeatBehavior = RepeatBehavior.Forever;
+        //            light.BeginAnimation(Ellipse.OpacityProperty, animation);
+        //        }
+        //    }
+        //}
+
 
     }
 }
