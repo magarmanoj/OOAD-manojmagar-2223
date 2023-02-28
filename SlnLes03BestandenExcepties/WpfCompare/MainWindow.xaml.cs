66a7e554bf1108e321b765237ebc79e26be6897c
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-02-28 22:22:34 +0100

    fix

--- SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
+++ SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
@@ -63,14 +63,25 @@ namespace WpfCompare
 
         private void BtnCompare_Click(object sender, RoutedEventArgs e)
         {
-            // loopt door al de items (woorden) van lbmsg1 en lbMsg2 en vergelijkt dezelfde items met elkaar om te zien of de value er van zelfde is 
-            // dus (vb woord gelijk - gelik) je ziet dat item = gelijk maar hun value is anders gelijk = 6 en gelik = 5 dus fout= rode kleur
-            for (int i = 0; i < lbMsg1.Items.Count; i++)
+            // // loopt door al de items (woorden) van lbmsg1 en lbMsg2 en vergelijkt dezelfde items met elkaar om te zien of de value er van zelfde is 
+            //    // dus (vb woord gelijk - gelik) je ziet dat item = gelijk maar hun value is anders gelijk = 6 en gelik = 5 dus fout= rode kleur
+            for (int i = 0; i < lbMsg2.Items.Count; i++)
             {
-                if (i < lbMsg2.Items.Count && lbMsg1.Items[i].ToString() != lbMsg2.Items[i].ToString())
+                if (i < lbMsg1.Items.Count && lbMsg1.Items[i].ToString() != lbMsg2.Items[i].ToString())
                 {
+                    // If the items don't match, set the background color to red
                     // https://stackoverflow.com/questions/22091772/how-to-change-background-color-of-selected-item-in-a-listbox-programmatically 
                     // zorgt voor het veranderen van backgound kleur, != item wordt in listBoxItem item toegevoegd en kleuren ervan wordt veranderd
+                    ListBoxItem item = lbMsg2.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
+                    if (item != null)
+                    {
+                        item.Background = Brushes.Red;
+                    }
+                }
+                else if (i >= lbMsg1.Items.Count)
+                {
+                    // If the index is greater than or equal to the number of items in lbMsg1,
+                    // set the background color to red for all the remaining items in lbMsg2
                     ListBoxItem item = lbMsg2.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
                     if (item != null)
                     {
