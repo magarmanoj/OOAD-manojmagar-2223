4eb975cb538edf017e4e2ae4bdd39ed16eafe5aa
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-03-25 19:43:45 +0100

    les6 vcard

---
+++ SlnLes06ClassesProperties/WpfVcardEditor/MyPopupWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfVcardEditor
+{
+    /// <summary>
+    /// Interaction logic for MyPopupWindow.xaml
+    /// </summary>
+    public partial class MyPopupWindow : Window
+    {
+        public MyPopupWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Sluiten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SlnLes06ClassesProperties/WpfVcardEditor/Vcard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfVcardEditor
+{
+    internal class Vcard
+    {
+
+        private string _gender;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender
+        {
+            get 
+            { 
+                return _gender; 
+            }
+            set
+            {
+                if (value == "F" || value == "M" || value == "O")
+                {
+                    _gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid gender. Gender must be either 'F', 'M', or 'O'.");
+                }
+            }
+        }
+        public string PEmail { get; set; }
+        public string PTelefoon { get; set; }
+        public BitmapImage Photo { get; set; }
+
+        // Werk
+        public string Bedrijf { get; set; }
+        public string JobTitel { get; set; }
+        public string WEmail { get; set; }
+        public string WTelefoon { get; set; }
+
+        // Sociaal
+        public string Linkedin { get; set; }
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string Youtube { get; set; }
+    }
+}
