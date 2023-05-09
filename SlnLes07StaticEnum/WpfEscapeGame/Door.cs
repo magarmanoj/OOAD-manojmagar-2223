595df582aff01349d950d427499ba432de049e14
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-05-09 21:08:41 +0200

    update

--- SlnLes07StaticEnum/WpfEscapeGame/Door.cs
+++ SlnLes07StaticEnum/WpfEscapeGame/Door.cs
@@ -2,5 +2,14 @@
 {
     internal class Door
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Room ToRoom { get; set; }
+
+        public Door(string name, string desc)
+        {
+            Name = name;
+            Description = desc;
+        }
     }
 }
