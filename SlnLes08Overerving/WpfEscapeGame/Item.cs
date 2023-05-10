5b5736c04281e56857d32d47946834a91820673b
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-05-10 09:50:53 +0200

    update

---
+++ SlnLes08Overerving/WpfEscapeGame/Item.cs
@@ -0,0 +1,28 @@
+﻿namespace WpfEscapeGame
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsLocked { get; set; } = false;
+        public bool IsPortable { get; set; } = true;
+        public Item Key { get; set; }
+        public Item HiddenItem { get; set; }
+        public Item(string name, string desc)
+        {
+            Name = name;
+            Description = desc;
+        }
+
+        public Item(string name, string desc, bool isPorttable)
+        {
+            Name = name;
+            Description = desc;
+            IsPortable = isPorttable;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ SlnLes08Overerving/WpfEscapeGame/Room.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Security.RightsManagement;
+
+namespace WpfEscapeGame
+{
+    public class Room
+    {
+        public string Name { get; } // read-only: kan maar één keer ingesteld worden
+        public string Description { get; }
+        public List<Item> Items { get; set; } = new List<Item>();
+        public List<Door> Doors { get; set; } = new List<Door>();
+        public string ImagePath { get; set; }
+
+        public Room(string name, string desc, string imagePath)
+        {
+            Name = name;
+            Description = desc;
+            ImagePath = imagePath;
+        }
+    }
+}
