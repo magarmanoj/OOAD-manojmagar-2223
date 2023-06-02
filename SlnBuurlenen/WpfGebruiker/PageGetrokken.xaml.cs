1e4346a1eb10bf4eea18d1f2b9078c6c73afad88
magarmanoj <113712069+magarmanoj@users.noreply.github.com>
2023-06-02 15:59:24 +0200

    final version

--- SlnBuurlenen/WpfGebruiker/PageGetrokken.xaml.cs
+++ SlnBuurlenen/WpfGebruiker/PageGetrokken.xaml.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace WpfGebruiker
